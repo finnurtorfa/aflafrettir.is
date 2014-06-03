621a97a0904e085c33ef78d68cd733af0d816aee
Finnur Torfason <finnurtorfa@gmail.com>
2014-06-03 13:55:26 +0200

    Use the correct template, and call the aflafrettir route decorator

--- app/aflafrettir/routes.py
+++ app/aflafrettir/routes.py
@@ -6,6 +6,6 @@ from . import aflafrettir
 def index():
   return render_template('aflafrettir/index.html')
 
-@talks.route('/user/<username>')
+@aflafrettir.route('/user/<username>')
 def user(username):
- return render_template('aflafrettir/index.html', username = username)
+ return render_template('aflafrettir/user.html', username = username)
