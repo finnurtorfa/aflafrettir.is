d06524e0e3ea6fd641376679128bf272c96deb5a
Finnur Torfason <finnurtorfa@gmail.com>
2014-06-03 10:00:03 +0200

    Register the aflafrettir blueprint with the app

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,7 @@ def create_app(config_name):
   app = Flask(__name__)
   app.config.from_object(config[config_name])
 
+  from .aflafrettir import aflafrettir as afla_blueprint
+  app.register_blueprint(afla_blueprint)
+
   return app
