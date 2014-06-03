b1468a29c9b2ef24a8e25dff3e3cb468a02684c4
Finnur Torfason <finnurtorfa@gmail.com>
2014-06-03 15:04:01 +0200

    Add a SQLAlchemy database uri to the config file

--- config.py
+++ config.py
@@ -1,17 +1,24 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class DevelopmentConfig(Config):
   DEBUG = True
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'my secret key'
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+          'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 class TestingConfig(Config):
   TESTING = True
+  SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+          'sqlite:///' + os.path.join(basedir, 'test-dev.sqlite')
 
 class ProductionConfig(Config):
-  pass
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+          'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 config = {
   'development': DevelopmentConfig,
