84ceb7e4b1f329b442c90e0a1020f8b817cd50e6
Finnur Torfason <finnurtorfa@gmail.com>
2014-06-03 17:46:42 +0200

    Add the login form to the auth routes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template
 
 from . import auth
+from .forms import LoginForm
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-  return render_template('auth/login.html')
+  form = LoginForm()
+
+  if form.validate_on_submit():
+      pass
+
+  return render_template('auth/login.html', form=form)
