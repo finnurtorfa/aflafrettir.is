b10921efcb3a7eb6c2c2302956b5bad121225d91
Finnur Torfason <finnurtorfa@gmail.com>
2014-06-03 19:40:01 +0200

    Log in the user on a POST request

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask.ext.login import login_user
 
 from . import auth
 from .forms import LoginForm
@@ -8,6 +9,14 @@ def login():
   form = LoginForm()
 
   if form.validate_on_submit():
-      pass
+    user = User.query.filter_by(email=form.email.data).first()
+
+    if user is None or not user.verify_password(form.password.data):
+      flash('Rangt notendanafn eða lykilorð')
+      return redirect(url_for('.login'))
+    
+    login_user(user, form.remember_me.data)
+
+    return redirect(request.args.get('next') or url_for('talks.index'))
 
   return render_template('auth/login.html', form=form)
