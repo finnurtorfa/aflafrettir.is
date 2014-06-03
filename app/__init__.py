74e240d3e2e397eb8f3b0e63a1666412c3c1c66b
Finnur Torfason <finnurtorfa@gmail.com>
2014-06-03 14:31:25 +0200

    Add flask-bootstrap to the mix

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+
 from config import config
+
+bootstrap = Bootstrap()
 
 def create_app(config_name):
   app = Flask(__name__)
   app.config.from_object(config[config_name])
+
+  bootstrap.init_app(app)
 
   from .aflafrettir import aflafrettir as afla_blueprint
   app.register_blueprint(afla_blueprint)
