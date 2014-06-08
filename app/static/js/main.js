655494895c20fd6c0c8de39df2401a4dd7a7983d
Finnur Torfason <finnurtorfa@gmail.com>
2014-06-08 15:41:40 +0200

    move styling files to a css subfolder and javascript files to static/js directory. Update the sidebar

---
+++ app/static/js/main.js
@@ -0,0 +1,16 @@
+$('[data-toggle=collapse]').click(function(){
+  var attr = $(this).attr("data-target");
+  $('a.collapsible[data-toggle=collapse]').each(function(){
+    var new_attr = $(this).attr("data-target");
+    if ( $(new_attr).hasClass("in")  && new_attr != attr ) {
+      $(new_attr).removeClass("in");
+      $(new_attr).fadeOut();
+    }
+  });
+  
+  if ( $(attr).hasClass("in") ) {
+    $(attr).fadeOut();
+  } else {
+    $(attr).fadeIn();
+  }
+});
