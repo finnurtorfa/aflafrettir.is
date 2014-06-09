5d78a0da7d24eb2dc4af648ece4e21cc2448b76e
Finnur Torfason <finnurtorfa@gmail.com>
2014-06-09 15:30:32 +0200

    Add a PostForm for posting a news story

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,5 +1,6 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import Required, Length, Email, Optional
 
 class ProfileForm(Form):
@@ -10,4 +11,11 @@ class ProfileForm(Form):
   bio       = TextAreaField('Um', validators=[Optional()])
   submit    = SubmitField('Breyta')
 
+class PostForm(Form):
+  title     = StringField('Titill', validators=[Required(), 
+                                                Length(1,64)])
+  created   = DateField('Dagsetning', validators=[Optional()])
+  post      = TextAreaField('Frétt', validators=[Required()])
+  category  = SelectField('Flokkur', coerce=int, validators=[Optional()])
+  submit    = SubmitField('Senda')
 
