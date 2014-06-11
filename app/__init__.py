908e9b9b72e3fdc6008820b2b14ff1f12487c8eb
Finnur Torfason <finnurtorfa@gmail.com>
2014-06-11 14:19:51 +0200

    Configure flask-uploads

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
 
 from config import config
 
@@ -9,6 +10,7 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+ads = UploadSet('ads', IMAGES)
 
 def create_app(config_name):
   app = Flask(__name__)
@@ -17,6 +19,7 @@ def create_app(config_name):
   bootstrap.init_app(app)
   db.init_app(app)
   login_manager.init_app(app)
+  configure_uploads(app, ads)
 
   from .aflafrettir import aflafrettir as afla_blueprint
   from .auth import auth as auth_blueprint
