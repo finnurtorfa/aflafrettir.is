12bf000631ede6dc4f73557d6f59947c43d854e6
Finnur Torfason <finnurtorfa@gmail.com>
2014-07-02 20:59:57 +0200

    Make sure the footer is at the bottom of the document. Make the text to news on the front page fade out. Fix the affix of the left sidebar in the admin section. Resize images on the front page

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -6,3 +6,34 @@ $('#content_left').affix({
     }
   }
 });	
+
+$('#admin_left').affix({
+  offset: {
+    top: 0,
+    bottom: function() {
+      return (this.bottom = $('footer').outerHeight(true))
+    }
+  }
+});	
+
+
+$(document).ready(function() {
+  var windowHeight   = $(window).height();
+  var documentHeight = $(document).height();
+  var footerHeight   = $('.footer').height();
+  var footerTop      = $('.footer').position().top + footerHeight;
+
+  if ( footerTop < windowHeight ) {
+    $('.footer').css('margin-top', (windowHeight - footerTop) + 'px');
+  } else if ( documentHeight > windowHeight && $('#admin_left').length ) {
+    $('.footer').css('margin-top', 60 + (documentHeight - footerTop) + 'px');
+  }
+
+  var img_width = $('div.img-resize').width();
+  $('img.img-resize').width(img_width);
+
+  $(window).resize(function() {
+    var img_width = $('div.img-resize').width();
+    $('img.img-resize').width(img_width);
+  });
+});
