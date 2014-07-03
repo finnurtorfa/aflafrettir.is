624a0feda056de4cba6d0e37110b4f3a72ecbbdc
Finnur Torfason <finnurtorfa@gmail.com>
2014-07-03 13:39:11 +0200

    Escape tables when truncating. Add a function and class to get extract the first image from a post. If there is an image in the body, use that as a thumbnai. Else use a default image

--- helpers/text.py
+++ helpers/text.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import unicodedata 
 
 from html.parser import HTMLParser
@@ -10,18 +11,36 @@ class HTMLStripper(HTMLParser):
     super(HTMLStripper, self).__init__()
     self.reset()
     self.fed = []
+    self.escape = False
 
   def handle_starttag(self, tag, attrs):
-    pass
+    if tag == 'table':
+      self.escape = True
 
   def handle_endtag(self, tag):
-    pass
+    if tag == 'table':
+      self.escape = False
 
   def handle_data(self, d):
-    self.fed.append(d)
+    if not self.escape:
+      self.fed.append(d)
 
   def get_data(self):
     return ''.join(self.fed)
+
+class HTMLThumbnailExtractor(HTMLParser):
+  def __init__(self):
+    super(HTMLThumbnailExtractor, self).__init__()
+    self.reset()
+    self.thumbnail = ''
+
+  def handle_starttag(self, tag, attrs):
+    if not self.thumbnail and tag == 'img':
+      for attr in attrs:
+        if attr[0] == 'src':
+          self.thumbnail = attr[1]
+          raise StopIteration
+
 
 def slugify(string):
   string = string.replace('æ', 'ae').replace('ð','d').replace('þ','th')\
@@ -43,3 +62,20 @@ def truncate(string, length=250, suffix=''):
     return string
   else:
     return ' '.join(string[:length+1].split(' ')[0:-1]) + suffix
+
+def get_thumbnail(html):
+  s = HTMLThumbnailExtractor()
+  try:
+    s.feed(html)
+  except StopIteration:
+    pass
+
+  if s.thumbnail:
+    f, e = os.path.splitext(s.thumbnail)
+    thumbnail = f + '_crop' + e
+    return os.path.split(thumbnail)
+
+  return os.path.split(s.thumbnail)
+    
+
+
