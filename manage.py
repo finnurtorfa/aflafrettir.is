e6bae2c0ce12ab4ebd56b2780acb2644d94eb072
Finnur Torfason <finnurtorfa@gmail.com>
2014-07-04 11:01:35 +0200

    Use flask-migrate for database migrations

--- manage.py
+++ manage.py
@@ -4,9 +4,12 @@ from app import create_app, db
 from app.models import User, Category
 
 from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def adduser(email, username, admin=False):
