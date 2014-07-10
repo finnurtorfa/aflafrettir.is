9fbc274bc2e4c19a67a232f0703912bbbc4567b0
Finnur Torfason <finnurtorfa@gmail.com>
2014-07-10 23:29:27 +0200

    Set up the contact form, route for it and template

--- app/aflafrettir/routes.py
+++ app/aflafrettir/routes.py
@@ -1,9 +1,13 @@
 import os
 
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect, request
+from flask.ext.mail import Message
 
 from . import aflafrettir
+from .forms import ContactForm
 from ..models import User, Category, Post, About
+
+from .. import mail
 
 from helpers.text import get_thumbnail, time_ago
 
@@ -62,6 +66,32 @@ def post(title, pid):
 def about():
   about = About.query.first()
   return render_template('aflafrettir/about.html', about=about)
+
+@aflafrettir.route('/hafa-samband', methods=['GET', 'POST'])
+def contact():
+  form = ContactForm()
+
+  if request.method == 'POST':
+    if not form.validate():
+      return render_template('aflafrettir/contact.html', form=form)
+    else:
+      msg = Message(form.subject.data, 
+                    sender=form.email.data,
+                    recipients=['finnurtorfa@gmail.com'],
+                    charset='utf-8')
+      print(mail)
+      msg.body = """
+      From: {n} <{e}>
+      {s}
+      """.format(n=form.name.data,
+                 e=form.email.data,
+                 s=form.message.data).encode('ascii', 'replace')
+
+      mail.send(msg)
+
+      return redirect(url_for('aflafrettir.contact'))
+
+  return render_template('aflafrettir/contact.html', form=form)
 
 @aflafrettir.route('/notandi/<username>')
 def user(username):
