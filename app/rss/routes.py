a0464b172071d745480857ec858e8991d6419369
Finnur Torfason <finnurtorfa@gmail.com>
2014-11-10 22:45:32 +0100

    Add the rss routes and blueprints

---
+++ app/rss/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template, current_app
+
+from . import feed
+from ..models import Post
+
+
+@feed.route('/', alias=True)
+@feed.route('/frettir')
+@feed.route('/frettir/<int:page>')
+def index(page=1):
+  posts = Post.get_per_page(page, current_app.config['POSTS_PER_PAGE'])
+
+  return render_template('rss/news.html',
+                          posts=posts)
+
+
+@feed.route('/frettir/flokkur/<int:cid>')
+@feed.route('/frettir/flokkur/<int:cid>/sida/<int:page>')
+def category(cid, page=1):
+  posts = Post.get_by_category(cid, page, current_app.config['POSTS_PER_PAGE'])
+
+  return render_template('rss/news.html',
+                          posts=posts)
