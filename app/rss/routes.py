2851ff1a1a52a80f71612ef9290fb4944cfc6084
Finnur Torfason <finnurtorfa@gmail.com>
2014-11-10 22:59:30 +0100

    Set the content type in the headers for the response of the rss feed

--- app/rss/routes.py
+++ app/rss/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, current_app
+from flask import render_template, current_app, after_this_request
 
 from . import feed
 from ..models import Post
@@ -8,6 +8,11 @@ from ..models import Post
 @feed.route('/frettir')
 @feed.route('/frettir/<int:page>')
 def index(page=1):
+  @after_this_request
+  def add_header(response):
+    response.headers['Content-type'] = 'text/xml'
+    return response
+
   posts = Post.get_per_page(page, current_app.config['POSTS_PER_PAGE'])
 
   return render_template('rss/news.html',
@@ -17,6 +22,11 @@ def index(page=1):
 @feed.route('/frettir/flokkur/<int:cid>')
 @feed.route('/frettir/flokkur/<int:cid>/sida/<int:page>')
 def category(cid, page=1):
+  @after_this_request
+  def add_header(response):
+    response.headers['Content-type'] = 'text/xml'
+    return response
+
   posts = Post.get_by_category(cid, page, current_app.config['POSTS_PER_PAGE'])
 
   return render_template('rss/news.html',
