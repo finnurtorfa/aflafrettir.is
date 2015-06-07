39819fd2203a4b906c92401bb93a7993d20e297b
Finnur Torfason <finnurtorfa@gmail.com>
2015-06-07 13:34:16 +0200

    Make a new upload set for the excel files

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_uploads import UploadSet, configure_uploads, IMAGES, DOCUMENTS
 from flask_mail import Mail
 from flask_whooshalchemy import whoosh_index
 from flask_babel import Babel
@@ -27,6 +27,7 @@ login_manager.login_view = 'auth.login'
 
 ads = UploadSet('ads', IMAGES)
 imgs = UploadSet('imgs', IMAGES)
+landings = UploadSet('landings', DOCUMENTS)
 
 def create_app(config_name):
   app = Flask(__name__)
@@ -41,7 +42,7 @@ def create_app(config_name):
   bootstrap.init_app(app)
   db.init_app(app)
   login_manager.init_app(app)
-  configure_uploads(app, (ads, imgs))
+  configure_uploads(app, (ads, imgs, landings))
   mail.init_app(app)
   babel.init_app(app)
   afla_manager.init_app(app)
